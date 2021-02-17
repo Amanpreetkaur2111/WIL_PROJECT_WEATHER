773a3afcb663552f7ab2c2d71bc0ce29c0bc8216
Amanpreet Kaur <aman021011@gmail.com>
2021-02-17 11:21:13 -0500

    Connection b/W Menu Page with Payment and Profile Page

--- app/src/main/java/com/example/we_protectapplication/ProfileActivity.java
+++ app/src/main/java/com/example/we_protectapplication/ProfileActivity.java
@@ -1,16 +1,56 @@
 package com.example.we_protectapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class ProfileActivity extends AppCompatActivity {
 
-
+    DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
     }
+
+
+    public void ClickMenu(View view){
+
+        // open drawer
+        MainActivity.openDrawer(drawerLayout);
+
+    }
+
+    public void ClickLogo(View view){
+
+        MainActivity.closeDrawer(drawerLayout);
+    }
+
+    public void ClickProfile(View view){
+
+        MainActivity.redirectActivity(this,MainActivity.class);
+    }
+
+    public void ClickPayment(View view){
+        recreate();
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        MainActivity.closeDrawer(drawerLayout);
+    }
+
+
+
+
+
+
 }
